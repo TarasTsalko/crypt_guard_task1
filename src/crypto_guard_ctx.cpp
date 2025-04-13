f25f0cc8443ec5da257a75e616fc925432febe76
Taras Tsalko <you@example.com>
2025-04-13 20:17:19 +0300

    Добавлена обработка ошибок из библиотеки openssl c использованием ERR_error_string_n

--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <vector>
+#include <openssl/err.h>
 
 namespace CryptoGuard {
 
@@ -36,6 +37,7 @@ public:
     };
 
     std::string CalculateChecksum(std::iostream &inStream) {
+        
         if (!inStream.good())
             throw std::ios_base::failure("Invalide input streams");
 
@@ -45,7 +47,7 @@ public:
         assert(md != nullptr);
 
         if (!EVP_DigestInit_ex2(mdctx.get(), md, nullptr))
-            throw std::runtime_error{"Message digest initialization failed."};
+            OpenSSLErrorHandling();
 
         std::vector<unsigned char> mdValue(EVP_MAX_MD_SIZE);
         std::string dataBlock;
@@ -53,11 +55,11 @@ public:
         while (inStream.good()) {
             inStream >> dataBlock;
             if (!EVP_DigestUpdate(mdctx.get(), dataBlock.data(), dataBlock.size()))
-                throw std::runtime_error{"Message digest update failed."};
+                OpenSSLErrorHandling();
         }
 
         if (!EVP_DigestFinal_ex(mdctx.get(), mdValue.data(), &messageLen))
-            throw std::runtime_error{"Message digest finalization failed"};
+            OpenSSLErrorHandling();
 
         std::stringstream res;
         for (unsigned int i = 0; i < messageLen; ++i)
@@ -68,6 +70,7 @@ public:
 
         return res.str();
     }
+
 private:
     AesCipherParams CreateChiperParamsFromPassword(std::string_view password) {
         AesCipherParams params;
@@ -85,6 +88,7 @@ private:
     }
 
     void IncryptDecryptImpl(std::iostream &inStream, std::iostream &outStream, std::string_view password, int mode) {
+        
         if (!inStream.good() || !outStream.good()) {
             throw std::ios_base::failure("Invalide input streams");
         }
@@ -96,7 +100,7 @@ private:
         params.encrypt = mode;
         // Инициализируем cipher
         if (!EVP_CipherInit_ex(ctx.get(), params.cipher, nullptr, params.key.data(), params.iv.data(), params.encrypt))
-            throw std::runtime_error("Openssl init error");
+            OpenSSLErrorHandling();
 
         unsigned int BLOCK_SIZE = 16;
         std::vector<unsigned char> inBuf(BLOCK_SIZE), outBuf(BLOCK_SIZE + EVP_MAX_BLOCK_LENGTH);
@@ -110,8 +114,9 @@ private:
                 inBuf[i] = ch;
                 part++;
             }
+
             if (!EVP_CipherUpdate(ctx.get(), outBuf.data(), &outLen, inBuf.data(), static_cast<int>(part)))
-                throw std::ios_base::failure("encrypting/decrypting error");
+                OpenSSLErrorHandling();
 
             for (int i = 0; i < outLen; ++i) {
                 outStream.put(outBuf[i]);
@@ -123,12 +128,21 @@ private:
         }
 
         if (!EVP_CipherFinal_ex(ctx.get(), outBuf.data(), &outLen))
-            throw std::ios_base::failure("finilize encrypting/decrypting error");
+            OpenSSLErrorHandling();
 
         for (int i = 0; i < outLen; ++i) {
             outStream.put(outBuf[i]);
         }
     }
+
+    void OpenSSLErrorHandling() const
+    {
+        unsigned long e = ERR_get_error();
+        std::vector<char> buff( 1024 );
+        ERR_error_string_n( e, buff.data(), buff.size() );
+        throw std::runtime_error( buff.data() );
+    }
+
 };
 
 CryptoGuardCtx::CryptoGuardCtx() : pImpl_(std::make_unique<CryptoGuardCtx::PImpl>()) {}
